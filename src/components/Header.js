987d91289de9f0960b698cc19be556ed6a1f683e
Thomas <thomashaudiquer@gmail.com>
2022-05-27 20:48:19 +0100

    navabar

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <div>
+            <nav className="header">
+               <img className="header-logo" src="./images/Troll Face.png" alt="" />
+               <h1 className="header-title">Meme Generator</h1> 
+               <p className="header-text">React Course - Project 3</p>
+            </nav>
+        </div>
+    )
+}
