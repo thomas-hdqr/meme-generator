2d231942e57954de8c195db5c096fc7c875f3a16
Thomas <thomashaudiquer@gmail.com>
2022-05-30 16:58:45 +0100

    injecting local data

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,26 @@
+import memesData from './memeData.js'
+
+
 export default function Meme() {
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+    }
+
     return (
-        <div>
-            <form className="meme">
+        <main>
+            <div className="meme">
                 <div className="meme-input">
                     <input className="meme-input-1" type="text" placeholder="Write someting"></input>
                     <input className="meme-input-2" type="text"placeholder="Write someting here too"></input>
                 </div>
                 <div>
-                    <input className="meme-button gradient" type="submit" value="Get a new meme image 🖼"></input>
+                    <button className="meme-button gradient" onClick={getMemeImage}>Get a new meme image 🖼</button>
                 </div>
-            </form>
-        </div>
+            </div>
+        </main>
     )
 }
