e62fc40f87f672098ec74595923acdc5e8a86666
Thomas <thomashaudiquer@gmail.com>
2022-06-01 17:12:44 +0200

    generating images

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,24 +1,31 @@
 import memesData from './memeData.js'
+import React, { useState } from "react";
+
 
 
 export default function Meme() {
 
+
+    const [memeImage, setMemeImage] = React.useState("")
+
     function getMemeImage() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
-        console.log(url)
+        setMemeImage(memesArray[randomNumber].url)
     }
 
     return (
         <main>
             <div className="meme">
                 <div className="meme-input">
-                    <input className="meme-input-1" type="text" placeholder="Write someting"></input>
-                    <input className="meme-input-2" type="text"placeholder="Write someting here too"></input>
+                    <input className="meme-input-1" type="text" placeholder="Top text"></input>
+                    <input className="meme-input-2" type="text"placeholder="Bottom text"></input>
                 </div>
                 <div>
                     <button className="meme-button gradient" onClick={getMemeImage}>Get a new meme image 🖼</button>
+                </div>
+                <div>
+                    <img src={memeImage} alt="" className="meme-image"/>
                 </div>
             </div>
         </main>
