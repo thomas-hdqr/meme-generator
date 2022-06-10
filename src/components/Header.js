b5ba806efe8cad82ea17edfa00ba4b7ad220406a
Thomas <thomashaudiquer@gmail.com>
2022-06-10 17:09:31 +0200

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
             <nav className="header gradient">
                <img className="header-logo" src="./images/Troll Face.png" alt="" />
                <h1 className="header-title">Meme Generator</h1> 
-               <p className="header-text">React Course - Project 3</p>
+               <p className="header-text">React Course - Project n3</p>
             </nav>
         </div>
     )
